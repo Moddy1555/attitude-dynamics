6f82b5508a100dd13d267304d9d4d177e6e41f89
Moddy1555 <andmod02@gmail.com>
2025-11-19 14:41:35 +0100

    Suggested changes

--- development_exercise_2/init.m
+++ development_exercise_2/init.m
@@ -1,17 +1,57 @@
+%% GROUP_01 DETAILS
+%  Authors:
+%  - Modena Andrea 2157605
+%  - Comunello Marco 2193512
+%  - De Guio Riccardo 2157243
+%  - Borghesan Alice 2196728
+
 clear; close all; clc;
 
-%  0) Satellite parameters
+%% Simulink Parameters
+dt = 0.01;
+tol = 1e-5;
 
-Ix = 0.0040;      
-Iy = 0.0050;       
-Iz = 0.0030;       
+%% Phase 0: Cubesat data and orbital parameters
+% Cubesat nU
+nX = 2;
+nY = 3;
+nZ = 4;
 
-omega0 = 0.001;   
+x = 0.1*nX;                     % [m]
+y = 0.1*nY;                     % [m]
+z = 0.1*nZ;                     % [m]
+m = 0.7*(nX + nY + nZ);         % [kg]
+
+Ix = m/12 * (z^2+y^2);          % [kg*m^2]
+Iy = m/12 * (z^2+x^2);          % [kg*m^2]
+Iz = m/12 * (x^2+y^2);          % [kg*m^2]
+
+I=[Ix  0  0;
+    0 Iy  0;
+    0  0 Iz];   
+
+% Orbital parameters
+
+re = 6371;                      % [km]
+r0 = re + 700;                  % [km]
+mu = 398600;                    % [km^3/s^2]
+
+T0 = 2*pi*sqrt(r0^3/mu);        % [s]
+
+q_IC=[0; 0; 0; 1];
+
+omega_OI = 2*pi/T0;             % [rad/s]
+omegaBI_IC = [0;-omega_OI;0];
+
+% Renaming omega_OI for convenience
+omega0 = omega_OI;
+
+% Calling s as tf object
 
 s = tf('s');
 
 
-%  1) Transfer Functions del sistema (Eq. 3)
+%% 1) Rewrite the transfer functions as specified in Eq. 3
 
 alpha_phi = 1/(omega0^2 * (Iz - Iy));
 alpha_psi = 1/(omega0^2 * (Iy - Ix));
@@ -20,57 +60,58 @@ G_phi   = -alpha_phi * 1 / (1 - alpha_phi*Ix*s^2);
 G_theta = 1 / (Iy * s^2);
 G_psi   =  alpha_psi * 1 / (1 + alpha_psi*Iz*s^2);
 
+%% 2) Translate the specifications (Eq. 7)
+% _star => required quantity
 
+k = 0;                              % [-]   system type (met by default)
+e_rp_star = 1e-2;                   % [rad] steady state error
+t_r_star = 60;                      % [s]   rise time
+m_phi_star = 60;                    % [deg] minimum phase margin
 
-%  2) Specifiche (Eq. 7)
+w_A_G_star = log(10) / t_r_star;    % [rad/s] open loop tf crossover frequency
 
-tr_star = 60;                         
-omega_AG_target = log(10) / tr_star;  
+%% Calculate the static gains of the controllers K (Eq. 6)
+K_phi_min = (1/alpha_phi) * (1/e_rp_star - 1);
+K_psi_min = (1/alpha_psi) * (1/e_rp_star - 1);
 
-%  3) Calcolo guadagni statici (Eq. 6)
-
-erp_star = 1e-2;
-
-K_phi_min = (1/alpha_phi) * (1/erp_star - 1);
-K_psi_min = (1/alpha_psi) * (1/erp_star - 1);
-
-
+% Initial iteration
 K_phi = abs(K_phi_min);
+K_theta = 1e-3;             % Suggested 0 < K << 1
 K_psi = abs(K_psi_min);
 
+%% 4) Verify the system stability requirements
 
-% Per theta K > 0 K << 1
-K_theta = 1e-3;
-
-
-%  4) Condizioni di stabilità
-
-if K_phi <= 0
-    warning("K_phi NON soddisfa le condizioni di stabilità!");
+if alpha_phi > 0 && K_phi < abs(1/alpha_phi)
+    warning("K_phi < abs(1/alpha_phi)");
 end
-if K_theta <= 0
-    warning("K_theta NON soddisfa le condizioni di stabilità!");
+if alpha_phi < 0 && K_phi < 0
+    warning("K_phi < 0");
 end
-if K_psi <= 0
-    warning("K_psi NON soddisfa le condizioni di stabilità!");
+if K_theta < 0
+    warning("K_theta < 0");
+end
+if alpha_psi > 0 && K_psi < 0
+    warning("K_phi < 0");
+end
+if alpha_psi < 0 && K_psi < abs(1/alpha_phi)
+    warning("K_phi < abs(1/alpha_phi)");
 end
 
 
-%  5) Definizione C(s)
+%% 5) Write the open-loop (C(s)G(s) and closed-loop (W(s)) transfer functions for the feedback system
 
-T1_phi = 800;     T2_phi = 0.1;
-T1_theta = 400;   T2_theta = 0.05;
-T1_psi = 900;     T2_psi = 0.1;
+T1_phi = 1000;     T2_phi = 0.1;
+T1_theta = 1000;   T2_theta = 0.1;
+T1_psi = 1000;     T2_psi = 0.1;
 
+% Controller (lead compensator) definitions
 C_phi   = K_phi   * (1 + T1_phi*s)   / (1 + T2_phi*s);
 C_theta = K_theta * (1 + T1_theta*s) / (1 + T2_theta*s);
 C_psi   = K_psi   * (1 + T1_psi*s)   / (1 + T2_psi*s);
 
-
-%  6) Open-loop chat?????? 
-
+% Open loop transfer functions
 L_phi   = C_phi   * G_phi;
-L_theta = C_theta * G_theta;
+L_theta = C_theta * G_theta; 
 L_psi   = C_psi   * G_psi;
 
 W_phi   = feedback(L_phi, 1);
@@ -78,32 +119,33 @@ W_theta = feedback(L_theta, 1);
 W_psi   = feedback(L_psi, 1);
 
 
-%  7) Bode diagrams + margin
+%% 6) Trace the Bode diagrams of the open-loop transfer function using MATLAB bode() function
 figure;
-bode(L_phi); grid on; title('Bode Diagram - φ axis');
+bode(L_phi); grid on; title('Bode Diagram - Roll axis');
 
 figure;
-bode(L_theta); grid on; title('Bode Diagram - θ axis');
+bode(L_theta); grid on; title('Bode Diagram - Pitch axis');
 
 figure;
-bode(L_psi); grid on; title('Bode Diagram - ψ axis');
+bode(L_psi); grid on; title('Bode Diagram - Yaw axis');
+
+%% 7) Adjust the controller parameters through trial and error using MATLAB's margin() function
+figure;
+margin(L_phi); grid on; title('Margin - Roll axis');
 
 figure;
-margin(L_phi); grid on; title('Margin - φ axis');
+margin(L_theta); grid on; title('Margin - Pitch axis');
 
 figure;
-margin(L_theta); grid on; title('Margin - θ axis');
+margin(L_psi); grid on; title('Margin - Yaw axis');
+
+
+%% 8) Verify the step response using MATLAB's step() function
+figure;
+step(W_phi); grid on; title('Step Response - Roll axis');
 
 figure;
-margin(L_psi); grid on; title('Margin - ψ axis');
-
-
-%  8) Step response
-figure;
-step(W_phi); grid on; title('Step Response - φ axis');
+step(W_theta); grid on; title('Step Response - Pitch axis');
 
 figure;
-step(W_theta); grid on; title('Step Response - θ axis');
-
-figure;
-step(W_psi); grid on; title('Step Response - ψ axis');
+step(W_psi); grid on; title('Step Response - Yaw axis');
